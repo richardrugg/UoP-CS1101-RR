f5aec962eb937399057f9a393aa9e6a43a9eaddd
richardrugg <richard@ruggcomputers.com>
2020-08-06 20:36:40 -0500

    Completed unit7-journal.py for submission.

--- unit7-journal.py
+++ unit7-journal.py
@@ -1,3 +1,5 @@
+"""
+# ORIGINAL SAMPLE GIVEN FOR INVERTING:
 def invert_dict(d):
     inverse = dict()
     for key in d:
@@ -7,3 +9,23 @@ def invert_dict(d):
         else:
             inverse[val].append(key)
     return inverse
+"""
+
+class_map = {'Core': ['ASR9K', 'NCS5501', 'NCS540'], 'Aggregate': ['ASR920', 'NCS540', 'C9500', 'ASR920'],
+             'Distribution': ['ASR920', 'C9500', 'C3850', 'C4948']}
+print('Original List: ', class_map)
+
+
+def invert_dict(d):
+    inverse = dict()
+    for key in d:
+        for item in d[key]:
+            if item not in inverse:
+                inverse[item] = [key]
+            else:
+                inverse[item].append(key)
+    return inverse
+
+
+inverse_class_map = invert_dict(class_map)
+print('Inverted List: ', inverse_class_map)
