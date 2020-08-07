c2fa88fed8e4e9b00d7841b3cf9f927441c6c206
richardrugg <richard@ruggcomputers.com>
2020-08-06 19:39:25 -0500

    Adding unit7-journal.py to repository.

---
+++ unit7-journal.py
@@ -0,0 +1,9 @@
+def invert_dict(d):
+    inverse = dict()
+    for key in d:
+        val = d[key]
+        if val not in inverse:
+            inverse[val] = [key]
+        else:
+            inverse[val].append(key)
+    return inverse
